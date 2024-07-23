31eb8620a0a22401f1593c7270e83f028a66f555
biswa3xam <biswa3xam@gmail.com>
2024-07-23 21:29:28 +0530

    image added to the card view and done some image background customization

--- app/src/main/java/com/biswa/composecomponents/MainActivity.kt
+++ app/src/main/java/com/biswa/composecomponents/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.biswa.composecomponents.ui.theme.ComposeComponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,23 +40,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            val painter = painterResource(id = R.drawable.jetpack_compose_logo_with_rocket)
+            ImageCard(
+                painter = painter,
+                title = "Jetpack Compose \nthis is not clickable",
+                contentDescription = "Jetpack compose"
+            )
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeComponentsTheme {
-        Greeting("Android")
+fun ImageCard(
+    painter: Painter, title: String, contentDescription: String, modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth(.5f)
+            .padding(top = 50.dp, start = 12.dp, bottom = 10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxHeight(.4f)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                modifier = modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = modifier.fillMaxSize().background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 700f,
+
+                )
+            ))
+            Text(
+                text = title,
+                style = TextStyle(color = Color.White, fontSize = 16.sp),
+                modifier = modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(start = 2.dp, bottom = 5.dp)
+            )
+        }
+
+
     }
 }
